ec080511b629f6d5b6bf1b45b1e5c0f1d880d00d
Harry Penlington <harry.penlington@gmail.com>
2020-08-08 05:11:27 +0100

    Problem 7 Solved

---
+++ problem7.py
@@ -0,0 +1,15 @@
+# Problem 7
+# 10001st Prime
+
+from utility.decorators import timeit, printit
+from utility.math_f import sieve_of_eratosthenes
+
+@printit
+@timeit
+def run():
+    x = sieve_of_eratosthenes(104750)
+    # x = sieve_of_eratosthenes(15)
+    return max(x), x.index(max(x))+1
+
+if __name__=="__main__":
+    run()
