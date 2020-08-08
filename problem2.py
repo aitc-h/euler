c8daf1b1488ac566062575177544aa24f9df88e9
Harry Penlington <harry.penlington@gmail.com>
2020-08-08 01:22:50 +0100

    Solved Problem 2 - Even Fibonacci Numbers

---
+++ problem2.py
@@ -0,0 +1,24 @@
+# Problem 2
+# Even Fibonacci Numbers
+
+from utility.timeit import timeit
+from utility.constants import golden
+
+def next_fib(i):
+    return round(i*golden)
+
+@timeit
+def run(lt):
+    terms = []
+    i = 1
+    while i < lt:
+        if i%2==0:
+            terms.append(i)
+        i = next_fib(i)
+    return sum(terms)
+
+
+if __name__=="__main__":
+    less_than = 4_000_000
+    
+    print(run(less_than))
