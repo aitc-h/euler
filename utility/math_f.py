ff68923f13630f4e0672c749da7052cffb22eec8
Harry Penlington <harry.penlington@gmail.com>
2020-08-10 14:41:20 +0100

    Add factorial function (and update formatting)

--- utility/math_f.py
+++ utility/math_f.py
@@ -1,42 +1,64 @@
 import math
 
-def is_prime(n):
+
+def factorial(n):
     if n <= 1:
-        return False
+        return 1
+    return n * factorial(n-1)
 
-    for i in range(2, n//2+1):
-        if n%i == 0:
-            return False
-    
-    return True
-
-def is_even(n):
-    return n&1 == 0
 
 def get_divisors(n):
     for i in range(1, int(n/2)+1):
-        if n%i==0:
+        if n % i == 0:
             yield i
     yield n
 
+
 def get_divisors_reversed(n):
     for i in range(int(n/2)+1, 1, -1):
-        if n%1==0:
+        if n % 1 == 0:
             yield i
     yield n
+
 
 def get_prime_divisors(n):
     factors = []
     while n % 2 == 0:
         factors.append(2)
         n /= 2
-    for i in range(3,int(math.sqrt(n))+1,2):
+    for i in range(3, int(math.sqrt(n))+1, 2):
         while n % i == 0:
             factors.append(int(i))
             n = math.floor(n / i)
     if n > 2:
         factors.append(int(n))
     return factors
+
+
+def is_even(n):
+    return n & 1 == 0
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, n//2+1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def sieve(MAX=100000):
+    prime = [True for i in range(MAX+1)]
+    k = int(math.sqrt(MAX))
+    for p in range(2, k):
+        if (prime[p]):
+            for i in range(2*p, MAX+1, p):
+                prime[i] = False
+    return prime
+
 
 def sieve_of_eratosthenes(n):
     prime = [True for i in range(n+1)]
@@ -52,14 +74,6 @@ def sieve_of_eratosthenes(n):
     nums = list(filter(lambda i: prime[i], range(n+1)))
     return nums
 
-def sieve(MAX=100000):
-    prime = [True for i in range(MAX+1)]
-    k = int(math.sqrt(MAX))
-    for p in range(2,k):
-        if (prime[p]):
-            for i in range(2*p, MAX+1, p):
-                prime[i] = False
-    return prime
 
 def sum_naturals_to_n(n):
     return int(n*(1+n)*(1/2))
