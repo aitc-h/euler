21adc2d0c9bbc58e29beac019a37a8798549dfd1
Harry Penlington <harry.penlington@gmail.com>
2020-08-10 14:41:00 +0100

    Update template.py

--- template.py
+++ template.py
@@ -1,12 +1,18 @@
-# Problem x
-# xxx
+"""
+    Problem x
+    xxx
+
+xxx
+"""
 
 from utility.decorators import timeit, printit
+
 
 @printit
 @timeit
 def run():
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     run()
